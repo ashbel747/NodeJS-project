d1158f4766b613f49758c125da71ee1de310148c
Ashbel <kashbel747@gmail.com>
2025-07-21 14:51:30 +0300

    Added Event Emmiter

---
+++ index.js
@@ -0,0 +1,17 @@
+const logEvents = require('./logEvents');
+
+const EventEmmitter = require('events');
+
+class MyEmitter extends EventEmmitter {};
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+//add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() =>{
+    //Emit Event
+    myEmitter.emit('log', 'Log event emitted!');
+}, 2000);
+
